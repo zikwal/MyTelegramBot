edbab838daef883d2f68111bf4a4712cd7482431
smartdeveloper <smartdevelopereth@gmail.com>
2017-12-03 22:39:00 +0300

    Удалил хлам

--- MyTelegramBot/Program.cs
+++ MyTelegramBot/Program.cs
@@ -21,16 +21,10 @@ namespace MyTelegramBot
 {
     public class Program
     {
-        private const string ssl = "req -newkey rsa:2048 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj";
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
 
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
-            string ddns = builder.Build().GetSection("DDNS").Value;
-            string token= builder.Build().GetSection("BotToken").Value;
             
         }
 
@@ -38,49 +32,6 @@ namespace MyTelegramBot
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-        private bool CreateSSL(string DDNS)
-        {
-            try
-            {
-                System.Diagnostics.Process.Start("\\Services\\OpenSSL\\bin\\openssl.exe", ssl + " \"" + "/C=US/ST=Russia/L=RUS/O=Bot Company/CN=" + DDNS + "\"");
-                return true;
-            }
-
-            catch
-            {
-                return false;
-            }
-        }
-
-        private async Task<WebhookInfo> GetWebHookInfo(string token)
-        {
-            try
-            {
-                TelegramBotClient telegram = new TelegramBotClient(token);
-                return await telegram.GetWebhookInfoAsync();
-            }
-
-            catch
-            {
-                return null;
-            }
-        }
-
-        private async Task<WebhookInfo> SetWebHook(string token, string DDNS)
-        {
-            try
-            {
-                TelegramBotClient telegram = new TelegramBotClient(token);
-                await telegram.SetWebhookAsync("https:/"+DDNS+"/api/values", new FileToSend("@", null), 50);
-                return await GetWebHookInfo(token);
-            }
-
-            catch
-            {
-                return null; 
-            }
-        }
 
         
     }
--- MyTelegramBot/Startup.cs
+++ MyTelegramBot/Startup.cs
@@ -21,7 +21,6 @@ namespace MyTelegramBot
 {
     public class Startup
     {
-        private const string ssl = "req -newkey rsa:2048 -sha256 -nodes -keyout YOURPRIVATE.key -x509 -days 365 -out YOURPUBLIC.pem -subj";
 
         public Startup(IConfiguration configuration)
         {
@@ -37,13 +36,8 @@ namespace MyTelegramBot
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
-            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
-            string token = builder.Build().GetSection("BotToken").Value;
-            string ddns = builder.Build().GetSection("DDNS").Value;
 
             if (env.IsDevelopment())
             {
@@ -52,59 +46,8 @@ namespace MyTelegramBot
            
             app.UseMvc();
 
-            //var info = await GetWebHookInfo(token);
-
-            //if (info.Url == "")
-            //    await SetWebHook(token, ddns);
         }
 
 
-        private async Task<WebhookInfo> GetWebHookInfo(string token)
-        {
-            try
-            {
-                TelegramBotClient telegram = new TelegramBotClient(token);
-                return await telegram.GetWebhookInfoAsync();
-            }
-
-            catch
-            {
-                return null;
-            }
-        }
-
-        private async Task<WebhookInfo> SetWebHook(string token, string DDNS)
-        {
-            try
-            {
-                DDNS = "311cf420.ngrok.io";
-                TelegramBotClient telegram = new TelegramBotClient(token);
-                var stream = GetSSL();
-                if(stream!=null)
-                    await telegram.SetWebhookAsync("https://"+DDNS+"/api/values", new FileToSend("@Cert", GetSSL()), 50);
-                else
-                    await telegram.SetWebhookAsync("https://" + DDNS + "/api/values", null, 50);
-
-                return await GetWebHookInfo(token);
-            }
-
-            catch (Exception exp)
-            {
-                return null; 
-            }
-        }
-
-        private System.IO.Stream GetSSL()
-        {
-            try
-            {
-                return System.IO.File.OpenRead("YOURPUBLIC.pem");
-            }
-
-            catch
-            {
-                return null;
-            }
-        }
     }
 }
